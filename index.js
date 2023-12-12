8e502d4cde38944021c42603b3c4cfd2b2b811a2
momtazrasel <momtazrasel8@gmail.com>
2023-12-12 15:00:59 +0600

    Web Page is created

--- index.js
+++ index.js
@@ -9,44 +9,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodejs_form'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodejs_form'
 });
 
 db.connect((err) => {
   if (err) {
     console.error('Error connecting to MySQL:', err);
-  } else {
-    console.log('Connected to MySQL database');
+    return;
   }
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  console.log('Connected to MySQL');
 });
 
 app.post('/submit', (req, res) => {
   const { firstName, lastName, phone, address, age } = req.body;
-
-  if (age.length === 2 && phone.length === 8 && !isNaN(age) && !isNaN(phone)) {
-    const query = 'INSERT INTO users (first_name, last_name, phone, address, age) VALUES (?, ?, ?, ?, ?)';
-    db.query(query, [firstName, lastName, phone, address, age], (err, result) => {
-      if (err) {
-        console.error('Error inserting data into database:', err);
-        res.sendStatus(500);
-      } else {
-        console.log('Data inserted into database');
-        res.sendStatus(200);
-      }
-    });
-  } else {
-    res.sendStatus(400);
-  }
+  const query = 'INSERT INTO users (first_name, last_name, phone, address, age) VALUES (?, ?, ?, ?, ?)';
+  db.query(query, [firstName, lastName, phone, address, age], (err, result) => {
+    if (err) {
+      console.error('Error inserting data into database:', err);
+      res.sendStatus(500);
+      return;
+    }
+    console.log('Data inserted into database');
+    res.sendStatus(200);
+  });
 });
 
 app.listen(port, () => {
